e9e407968b16e4e326fbca389f2e9c86544c2176
Prince <princetripathi093@gmail.com>
2024-04-14 13:15:47 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -47,4 +47,9 @@ app.use("/api/ratings", ratingRouter);
 const adminOrderRoutes = require("./routes/adminOrder.routes.js");
 app.use("/api/admin/orders", adminOrderRoutes);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 module.exports = { app };
